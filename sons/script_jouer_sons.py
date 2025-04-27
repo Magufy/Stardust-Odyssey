0bd4c26e9c420742a75f3c0cff0fde5dbd4d6a75
AviationAY1574 <166242262+AviationAY1574@users.noreply.github.com>
2025-04-27 10:27:02 +0200

    Update script_jouer_sons.py

--- sons/script_jouer_sons.py
+++ sons/script_jouer_sons.py
@@ -1,15 +1,15 @@
 from playsound import playsound
 damage_to_player = 0
 
-
 son1 = "Musique menu principal - 1.mp3"
 son2 = "Menu - 2.mp3"
 son3 = "son de laser"
 son4 = "Musique-vague-1.mp3"
 son5 = "Musique-vague-2.mp3"
-
-
-
+son6 = "Musique-vague-3.mp3"
+son7 = "Musique-vague-4.mp3"
+son8 = "Musique-vague-5.mp3"
+son9 = "Musique-vague-6.mp3"
 
 
 
@@ -33,9 +33,23 @@ def jouer_son():
     if damage_to_player < global damage_to_player :
       playsound("lifelose.mp3")
       damage_to_player = global damage_to_player
-  if wave_number == 1 :
+  for wave_number in range 1000:
+  if wave_number == 0:
     playsound(son4)
-  if wave_number == 2 :
+  if wave_number == 1:
     playsound(son5)
+  if wave_number == 2 :
+    playsound(son6)
+  if wave_number == 3:
+    playsound(son7)
+  if wave_number > 3 and wave_number%2 == 0 :
+    playsound(son8)
+  if wave_number > 3 and wave_number%2 != 0:
+    playsound(son9)
+    
+    
   
+
+
+
     
