ebaa721aff70e8fe375a5e54a32e4f68226d1308
AviationAY1574 <166242262+AviationAY1574@users.noreply.github.com>
2025-04-27 10:01:22 +0200

    Update script_jouer_sons.py

--- sons/script_jouer_sons.py
+++ sons/script_jouer_sons.py
@@ -1,8 +1,12 @@
 from playsound import playsound
+damage_to_player = 0
+
 
 son1 = "Musique menu principal - 1.mp3"
 son2 = "Menu - 2.mp3"
-
+son3 = "son de laser"
+son4 = "Musique-vague-1.mp3"
+son5 = "Musique-vague-2.mp3"
 
 
 
@@ -22,4 +26,16 @@ def jouer_son():
   if shoot() == True :
     playsound("son de tir")
   if explode() == True :
-    playsound(
+    playsound("explosion1.mp3")
+  if check_collision() == True :
+    playsound("explosion1.mp3")
+  def check_damage_increase():
+    if damage_to_player < global damage_to_player :
+      playsound("lifelose.mp3")
+      damage_to_player = global damage_to_player
+  if wave_number == 1 :
+    playsound(son4)
+  if wave_number == 2 :
+    playsound(son5)
+  
+    
