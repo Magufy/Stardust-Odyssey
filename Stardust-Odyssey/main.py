87ad9cc9e45b77d87e779fe658baa083bc503b3d
POufi12 <francoisbouvier75@icloud.com>
2025-05-09 12:04:08 +0200

    Update main.py

--- Stardust-Odyssey/main.py
+++ Stardust-Odyssey/main.py
@@ -7,21 +7,7 @@ pygame.init()
 # Paramètres de la fenêtre en plein écran
 window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 WIDTH, HEIGHT = window.get_size()
-pygame.display.set_caption("Nova Drift Prototype - Fullscreen Mode")
-
-# Couleurs
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-GRAY = (128, 128, 128)
-BLACK = (0, 0, 0)
-
-selected_ship = "Vaisseau Basique"
-
-# Police
-font = pygame.font.Font(None, 36)
+pygame.display.set_caption("Stardust Odyssey")
 
 
 Shop().run(window)
