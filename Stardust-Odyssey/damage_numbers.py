6b7ca6072fbfa58946529578861e932dcad4a56e
AviationAY1574 <166242262+AviationAY1574@users.noreply.github.com>
2025-05-14 19:24:08 +0200

    Update damage_numbers.py

--- Stardust-Odyssey/damage_numbers.py
+++ Stardust-Odyssey/damage_numbers.py
@@ -3,8 +3,10 @@ import random
 
 class DamageNumber:
     def __init__(self, x, y, damage, is_critical=False):
+        # position de départ de l'objet texte
         self.x = x
         self.y = y
+        # Valeur des dégâts à afficher
         self.damage = damage
         self.time_left = 45  # Durée d'affichage en frames
         self.color = (255, 165, 0) if is_critical else (255, 255, 255)  # Orange pour critique, blanc sinon
@@ -14,16 +16,19 @@ class DamageNumber:
         
     def update(self):
         self.y -= self.speed  # Monte
-        self.time_left -= 1
+        self.time_left -= 1 # Réduit le temps restant d'affichage
         return self.time_left > 0  # Retourne True si le nombre doit rester affiché
         
     def draw(self, window):
         # Rendre le texte avec une taille plus grande pour les critiques
         text = self.font.render(str(int(self.damage)), True, self.color)
+        # Centre le texte horizontalement par rapport à pos_x
         window.blit(text, (self.pos_x - text.get_width() // 2, self.y))
 
 class DamageNumberManager:
+    
     def __init__(self):
+        # Liste de tous les DamageNumbers affichés
         self.damage_numbers = []
         
     def add_damage_number(self, x, y, damage, is_critical=False):
@@ -34,5 +39,6 @@ class DamageNumberManager:
         self.damage_numbers = [num for num in self.damage_numbers if num.update()]
         
     def draw(self, window):
+        # Affiche nombre de dégats 
         for number in self.damage_numbers:
             number.draw(window)
